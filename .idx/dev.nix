be1842ed749468fb9fdf54263e587d1e657d5057
aye.dev.env <aye.dev.env@gmail.com>
2024-06-03 11:32:41 +0000

    coba

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,13 +4,14 @@
     pkgs.nodejs_20
   ];
   env = {
-    NEXT_PUBLIC_FIREBASE_API_KEY = "api-key";
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN = "auth-domain";
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID = "project-id";
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET = "storage-bucket";
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID = "sender-id";
-    NEXT_PUBLIC_FIREBASE_APP_ID = "app-id";
-    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID = "analytic-id";
+    NEXT_PUBLIC_FIREBASE_API_KEY = "AIzaSyDNBjZU5Ar0_bae2y9o3Eir87eRBeKr1Wg";
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN = "gudut5.firebaseapp.com";
+    NEXT_PUBLIC_FIREBASE_DATABASE_URL = "https://gudut5-default-rtdb.asia-southeast1.firebasedatabase.app";
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID = "gudut5";
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET = "gudut5.appspot.com";
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID = "150974475008";
+    NEXT_PUBLIC_FIREBASE_APP_ID = "1:150974475008:web:e50591faf3600122bf61e5";
+    
   };
   idx.extensions = [
     
