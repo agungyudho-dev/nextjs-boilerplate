c64f1e538a9ac2ba4267faa85be36cea911512fc
aye.dev.env <aye.dev.env@gmail.com>
2024-06-03 10:50:51 +0000

    rollback

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,13 +4,13 @@
     pkgs.nodejs_20
   ];
   env = {
-    REACT_APP_FIREBASE_API_KEY = "api-key";
-    REACT_APP_FIREBASE_AUTH_DOMAIN = "auth-domain";
-    REACT_APP_FIREBASE_PROJECT_ID = "project-id";
-    REACT_APP_FIREBASE_STORAGE_BUCKET = "storage-bucket";
-    REACT_APP_FIREBASE_MESSAGING_SENDER_ID = "sender-id";
-    REACT_APP_FIREBASE_APP_ID = "app-id";
-    REACT_APP_FIREBASE_MEASUREMENT_ID = "analytic-id";
+    NEXT_PUBLIC_FIREBASE_API_KEY = "api-key";
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN = "auth-domain";
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID = "project-id";
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET = "storage-bucket";
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID = "sender-id";
+    NEXT_PUBLIC_FIREBASE_APP_ID = "app-id";
+    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID = "analytic-id";
   };
   idx.extensions = [
     
